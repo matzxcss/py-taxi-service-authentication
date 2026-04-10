218918038257d908b1e9e369d18c411f18013d7a
matzxcss <matheuslacerda01@proton.me>
2026-04-10 12:02:08 -0300

    fix errors

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -109,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 AUTH_USER_MODEL = "taxi.Driver"
 
 LOGIN_REDIRECT_URL = "taxi:index"
-LOGOUT_REDIRECT_URL = "templates/registration/login.html"
+LOGOUT_REDIRECT_URL = "login"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
